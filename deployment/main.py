40bc7534a3d0ea752be13f847be9434f0be8211b
Manoj Kumar R <manoj2003r@gmail.com>
2024-10-05 14:08:52 +0530

    Added Graph Functionality

--- deployment/main.py
+++ deployment/main.py
@@ -3,7 +3,7 @@ import subprocess
 
 st.title("Document Processing Tool")
 
-option = st.selectbox("Choose the processing type:", ("select type","Table Extraction", "Text Extraction"))
+option = st.selectbox("Choose the processing type:", ("select type","Table Extraction", "Text Extraction", "Graph Extraction"))
 
 if option == "Table Extraction":
     st.write("You selected Table Extraction.")
@@ -13,3 +13,7 @@ elif option == "Text Extraction":
     st.write("You selected Text Extraction.")
     st.write("Running Text Extraction script...")
     subprocess.run(["streamlit", "run", "app3.py"])
+elif option == "Graph Extraction":
+    st.write("You selected Graph Extraction.")
+    st.write("Running Graph Extraction script...")
+    subprocess.run(["streamlit", "run", "StreamlitGraph.py"])
